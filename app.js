4d6ae2e771cd2e7bc10f9c11674638b5b941dc0c
SerKasai <serjtheignis92@gmail.com>
2025-04-10 17:00:35 +0200

    finish

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const lista = document.getElementById('lista')
 console.log(lista)
 
-for (let i = 1; i < 101; i++) {
-  const li = document.createElement('li');
-  console.dir(li);
-  li.classList.add('box--normale');
-  li.append(i);
-  lista.append(li);
+for (let i = 1; i <= 100; i++) {
+  const div = document.createElement('div');
+  console.dir(div);
+  div.classList.add('box--normale');
+  // div.append(div);
+  // lista.append(div);
 
-  if (i % 3 === 0) {
-    li.classList.add('box--fizz');
-    li.append(`fizz`)
+  if (i % 3 === 0 && i % 5 === 0) {
+    div.classList.add('box--fizzbuzz');
+    div.append(`fizzbuzz`)
+
+  } else if (i % 3 === 0) {
+    div.classList.add('box--fizz');
+    div.append(`fizz`)
+
+  } else if (i % 5 === 0) {
+    div.classList.add('box--buzz');
+    div.append(`buzz`)
+
+  } else {
+    div.append(i);
   }
 
-  if (i % 5 === 0) {
-    li.classList.add('box--buzz');
-    li.append(`buzz`)
-  }
+  lista.append(div);
 
-  if (i % 15 === 0) {
-    li.classList.add('box--fizzbuzz');
-  }
 }
